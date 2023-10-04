a0187b67328c080590a79791f4d5dfa32ba81ff4
working <57171759+NLGithubWP@users.noreply.github.com>
2023-10-04 21:01:50 +0800

    Add py interface

--- pg_interface.py
+++ pg_interface.py
@@ -114,8 +114,8 @@ def model_inference_compute(params: dict, args: Namespace):
 
     begin = time.time()
     y = sliced_model(transformed_data, None)
-    logger.info(f"Prediction Results = {y.tolist()[:10]}")
-    
+    logger.info(f"Prediction Results = {y.tolist()[:2]}...")
+
     time_usage_dic["compute"] = time.time() - begin
     time_usage_dic["data_fetch"] = params['spi_seconds']
 
